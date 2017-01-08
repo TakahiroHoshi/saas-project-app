61f54d6c3032be1b211bc05f9c6ba4deb837adf0
TakahiroHoshi <loinzaft@gmail.com>
2017-01-08 18:21:57 +0900

    commit chnages

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -19,7 +19,7 @@ class Artifact < ActiveRecord::Base
     end
 
     def upload_to_s3
-      s3 = Aws::S3::Resource.new(region: ENV['AWS_REGION'])
+      s3 = Aws::S3::Resource.new(:region => ENV['AWS_REGION'])
       tenant_name = Tenant.find(Thread.current[:tenant_id]).name
       obj = s3.bucket(ENV['S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
       obj.upload_file(upload.path, acl:'public-read')
